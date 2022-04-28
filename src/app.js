3a776bd42111d0be652b428d03eec56ce0549d86
Lital Kolenberg Geron <lital88@gmail.com>
2022-04-28 10:23:35 +0300

    added error functions

--- src/app.js
+++ src/app.js
@@ -1,5 +1,14 @@
 if (process.env.USER) require("dotenv").config();
 const express = require("express");
+const errorHandler = require("./errors/errorHandler");
+const notFound = require("./errors/notFound");
+
+
 const app = express();
+
+app.use(express.json());
+
+app.use(notFound);
+app.use(errorHandler);
 
 module.exports = app;
